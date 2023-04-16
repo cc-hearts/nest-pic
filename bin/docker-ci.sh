cda3d32e08b196dd877fe34834fb224f9b10b4ad
heart <7362469@qq.com>
2023-04-16 21:26:45 +0800

    chore: update docker-ci.sh

---
+++ bin/docker-ci.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+docker rm -f nest-pic
+docker rmi -f nest-pic:1.0.0
+
+docker build -t nest-pic:1.0.0 .
+
+docker run -p 80:80 -v /opt/nest-pic/nest-pic/file:/usr/nest-pic/file --name nest-pic -d nest-pic:1.0.0
